cf11094c5f04c2a5e99a9b0eb3a085a621971744
Tisha Kedar <23amtics147@gmail.com>
2025-08-16 17:30:12 +0530

    develop navbar and make it responsive as well

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  NavbarWrapper,
+  NavbarContainer,
+  Logo,
+  NavLinks,
+  NavItem,
+  NavbarMobileMenuIcon,
+  NavbarMobileMenu,
+  NavbarMobileItem,
+  Anchor,
+  CTAEnquiryNow,
+  Divider,
+} from "./styles";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <NavbarWrapper>
+      <NavbarContainer>
+        <Logo src="/assets/logo.svg" />
+        <NavLinks>
+          <NavItem to="/">Home</NavItem>
+          <NavItem to="/about">About</NavItem>
+          <NavItem to="/contact">Contact</NavItem>
+          <NavItem to="/categories">Categories</NavItem>
+          <Anchor href="#">
+            <CTAEnquiryNow>Enquiry now</CTAEnquiryNow>
+          </Anchor>
+        </NavLinks>
+
+        <NavbarMobileMenuIcon
+          onClick={toggleMenu}
+          src={isMenuOpen ? "/assets/close_circle.svg" : "/assets/menu.svg"}
+        />
+        <NavbarMobileMenu className={isMenuOpen ? "active" : ""}>
+          <NavbarMobileItem to="/" onClick={toggleMenu}>
+            Home
+          </NavbarMobileItem>
+          <Divider />
+          <NavbarMobileItem to="/about" onClick={toggleMenu}>
+            About
+          </NavbarMobileItem>
+          <Divider />
+          <NavbarMobileItem to="/contact" onClick={toggleMenu}>
+            Contact
+          </NavbarMobileItem>
+          <Divider />
+          <NavbarMobileItem to="/categories" onClick={toggleMenu}>
+            Categories
+          </NavbarMobileItem>
+          <Anchor href="#">
+            <CTAEnquiryNow>Enquiry now</CTAEnquiryNow>
+          </Anchor>
+        </NavbarMobileMenu>
+      </NavbarContainer>
+    </NavbarWrapper>
+  );
+};
 
 export default Navbar;
